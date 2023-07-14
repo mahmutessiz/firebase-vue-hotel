fff4a81302066f6c40767ccccabb45218d091079
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-07-14 17:31:18 +0300

    add delete function

--- src/components/RoomsContainer.vue
+++ src/components/RoomsContainer.vue
@@ -7,9 +7,17 @@
         class="relative grid h-60 w-60 place-items-center border-2"
       >
         <div class="absolute h-full w-full bg-slate-500/50" v-if="room.isOccupied == 'true'"></div>
+        <div class="absolute h-full w-full bg-blue-500/50" v-if="room.isOccupied == 'false'"></div>
         {{ room.roomNumber }} <br />
         {{ room.isOccupied }} <br />
-        {{ room.roomId }}
+        {{ room.roomId }} <br />
+        <button
+          id="delete-button"
+          class="cursor-pointer rounded-md bg-red-500 px-2 py-1 text-white shadow-md"
+          @click="() => deleteRoom(room.roomId)"
+        >
+          deleteRoom
+        </button>
       </li>
     </ul>
   </div>
@@ -21,13 +29,12 @@ import { onMounted, ref } from 'vue'
 /**
  * !Firebase
  */
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
 import { db } from '@/firebase'
 
 let rooms = ref([])
-
 /**
- * ? Order room by room numbers
+ * Order room by room numbers
  */
 function sortByRoomNumber(arr) {
   return arr.sort((a, b) => a.roomNumber - b.roomNumber)
@@ -44,4 +51,18 @@ onMounted(async () => {
   })
   rooms.value = sortByRoomNumber(rooms.value)
 })
+
+/* const deleteRoom = async () => {
+  const deleteButton = document.querySelector('#delete-button')
+  console.log(deleteButton.parentElement.innerText)
+   await deleteDoc(doc(db, 'rooms', 'v9tiAXr0UwNspva5prRP'), err => {console.log(err);})
+}
+ */
+const deleteRoom = async (roomId) => {
+  try {
+    await deleteDoc(doc(db, 'rooms', roomId))
+  } catch (err) {
+    console.error(err)
+  }
+}
 </script>
