14102798a73ad8e67b3685d5dfea77333cdd1963
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-07-16 16:01:34 +0300

    signIn method

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-error
-    </div>
+  <div>error</div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid min-h-screen w-full place-content-center">
-    <form action="" class="flex flex-col gap-2">
+    <form action="" class="flex flex-col gap-2" @submit="logIn" id="form">
       <label for="email">e-mail</label>
       <input
         type="email"
@@ -13,10 +13,11 @@
         id="password"
         class="rounded-md border border-black px-2 shadow-sm shadow-black/20"
       />
-      <button class="rounded-md bg-gray-600 px-2 py-1 text-white hover:bg-slate-700" @click="logIn">
+      <button class="rounded-md bg-gray-600 px-2 py-1 text-white hover:bg-slate-700">
         sign in
       </button>
     </form>
+    <p class="w-[80%]">{{ userData }}</p>
   </div>
 </template>
 
@@ -24,25 +25,36 @@
 /**
  * !Firebase
  */
-import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
-
+/* import {  doc, addDoc } from 'firebase/firestore' */
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { db, auth } from '@/firebase'
-import { signInWithEmailAndPassword } from 'firebase/auth'
 
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-
+import { ref } from 'vue'
+let userData = ref([])
 const logIn = (e) => {
   // Create a new user
   e.preventDefault()
+  const form = document.querySelector('#form')
+  const email = form['email'].value
+  const password = form['password'].value
+  console.log(email, password)
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user
+  // Create the user in Firebase Auth
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((user) => {
+      console.log('User created:', user, user.uid)
+      userData.value.push(user)
+      // Create a document in the users collection
+
+      /* addDoc(doc(db, 'hotels', user.uid), {
+        name: user.displayName,
+        email: user.email,
+        createdAt: new Date()
+      }) */
+      console.log(userData)
     })
     .catch((error) => {
-      console.log(error.code, error.message)
+      console.log('Error creating user:', error)
     })
 }
 </script>
