aa0f3731709da15d69217b673e74fd5d72366c77
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-07-27 16:44:39 +0300

    change error in console

--- src/components/AddOptionsPopup.vue
+++ src/components/AddOptionsPopup.vue
@@ -25,13 +25,10 @@ let currentUser = null
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
     currentUser = user.uid
     // ...
   } else {
     // User is signed out
-    // ...
   }
 })
 function addRoom() {
