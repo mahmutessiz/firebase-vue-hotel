e7f6d733f7c090aa2e78fbf733ced2fd9ffb6d74
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-08-20 17:36:45 +0300

    Revert "."
    This reverts commit b87bdfbabdf7125a19454985021fd3ccb85ad93d.

--- src/components/RoomsContainer.vue
+++ src/components/RoomsContainer.vue
@@ -1,90 +1,5 @@
-<script setup>
-import WelcomePageNotLoggedIn from "./WelcomePageNotLoggedIn.vue"
-// Firebase imports
-import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore'
-import { db, auth } from '@/firebase'
-import { onAuthStateChanged } from 'firebase/auth'
-
-// !Get room information from the server
-let rooms = ref([])
-let userUid = ref('')
-let isLoggedIn = ref(false)
-
-//Order room by room numbers
-function sortByRoomNumber(arr) {
-  return arr.sort((a, b) => a.roomNumber - b.roomNumber)
-}
-
-//listen user login status
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    const roomRef = collection(db, user.uid)
-    onSnapshot(roomRef, (snapshot) => {
-      rooms.value = []
-      snapshot.forEach((doc) => {
-        rooms.value.push({
-          roomId: doc.id,
-          isOccupied: doc.data().isOccupied,
-          roomNumber: doc.data().roomNumber
-        })
-      })
-      rooms.value = sortByRoomNumber(rooms.value)
-      isLoggedIn.value = true
-    })
-    userUid.value = user.uid
-  } else {
-    rooms.value = []
-    isLoggedIn.value = false
-    console.log('user signed out')
-  }
-})
-
-// !Delete room function
-const deleteRoom = async (userId, roomId) => {
-  try {
-    await deleteDoc(doc(db, userId, roomId))
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-// Change room status function
-const changeStatus = async (e, roomId) => {
-  e.preventDefault()
-  const form = document.querySelector(`[data-room-id="${roomId}"] #status-form`)
-  const roomStatus = form['room-status'].value
-  if (roomStatus === '') return // check if value is empty
-  try {
-    await setDoc(
-      doc(db, userUid.value, roomId),
-      {
-        isOccupied: roomStatus
-      },
-      { merge: true }
-    )
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-// Change room statuspopup function
-const statusPopup = (roomId) => {
-  const formPopup = document.querySelector(`[data-room-id="${roomId}"]`)
-  if (formPopup.classList.contains('hidden')) {
-    formPopup.classList.remove('hidden')
-    formPopup.classList.add('grid')
-  } else if (formPopup.classList.contains('grid')) {
-    formPopup.classList.remove('grid')
-    formPopup.classList.add('hidden')
-  }
-}
-</script>
-
 <template>
-  <div
-    class="flex w-full flex-col items-center justify-center bg-green-500 px-2 py-4"
-    v-if="isLoggedIn == true"
-  >
+  <div class="flex w-full flex-col items-center justify-center bg-green-500 px-2 py-4">
     <ul class="flex flex-wrap justify-center gap-4 p-4">
       <li
         v-for="room in rooms"
@@ -103,31 +18,61 @@ const statusPopup = (roomId) => {
         >
           deleteRoom
         </button>
-
-        <button @click="statusPopup(room.roomId)" class="z-40 cursor-pointer">
-          <img src="../assets/options.svg" alt="options" />
-        </button>
-        <!-- Options popup -->
-        <div
-          class="absolute inset-0 z-50 hidden w-full place-items-center bg-white/90"
-          :data-room-id="room.roomId"
-          id="status-form-container"
-        >
-          <form action="" @submit="changeStatus($event, room.roomId)" id="status-form">
-            <p>Please select room status:</p>
-            <input type="radio" id="true" name="room-status" value="true" />
-            <label for="html">Occupied</label><br />
-            <input type="radio" id="false" name="room-status" value="false" />
-            <label for="css">Not Occupied</label><br />
-            <button class="mt-4 rounded-md bg-green-500 px-2 py-1 shadow-sm shadow-black/80">
-              Apply
-            </button>
-          </form>
-        </div>
       </li>
     </ul>
   </div>
-  <div v-if="isLoggedIn == false">
-    <WelcomePageNotLoggedIn />
-  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import WelcomePageNotLoggedIn from './WelcomePageNotLoggedIn.vue'
+
+// Firebase imports
+import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore'
+import { db, auth } from '@/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+
+// !Get room information from the server
+let rooms = ref([])
+let userUid = ref('')
+
+//Order room by room numbers
+function sortByRoomNumber(arr) {
+  return arr.sort((a, b) => a.roomNumber - b.roomNumber)
+}
+
+/**
+ * !listen user login status
+ */
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const roomRef = collection(db, user.uid)
+    onSnapshot(roomRef, (snapshot) => {
+      rooms.value = []
+      snapshot.forEach((doc) => {
+        rooms.value.push({
+          roomId: doc.id,
+          isOccupied: doc.data().isOccupied,
+          roomNumber: doc.data().roomNumber
+        })
+      })
+      rooms.value = sortByRoomNumber(rooms.value)
+    })
+    userUid.value = user.uid
+  } else {
+    rooms.value = []
+    console.log('user signed out')
+  }
+})
+
+/**
+ * !Delete room function
+ */
+const deleteRoom = async (userId, roomId) => {
+  try {
+    await deleteDoc(doc(db, userId, roomId))
+  } catch (err) {
+    console.error(err)
+  }
+}
+</script>
--- src/components/WelcomePageNotLoggedIn.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="grid h-screen w-full place-items-center text-red-400">
-    You must sign in to see the console
-  </div>
-</template>
-
-<script setup></script>
