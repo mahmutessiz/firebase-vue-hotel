0a47ea5fad29f175036d8953929d553a24b184b3
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-08-21 17:28:14 +0300

    add status change func

--- src/components/RoomsContainer.vue
+++ src/components/RoomsContainer.vue
@@ -4,20 +4,44 @@
       <li
         v-for="room in rooms"
         :key="room"
-        class="relative grid h-80 w-72 place-items-center overflow-hidden rounded-md bg-white shadow-lg shadow-black/40"
+        class="relative flex h-80 w-72 items-center justify-center gap-2 overflow-hidden rounded-md bg-white shadow-lg shadow-black/40"
       >
+        <div class="z-50 flex flex-col">
+          <button
+            id="delete-button"
+            class="z-10 cursor-pointer rounded-md bg-red-500 px-2 py-1 text-white shadow-md"
+            @click="() => deleteRoom(userUid, room.roomId)"
+          >
+            Dell
+          </button>
+
+          <button @click="statusPopup(room.roomId)" class="z-40 cursor-pointer">
+            <img src="../assets/options.svg" alt="options" />
+          </button>
+          <!-- Options popup -->
+          <div
+            class="absolute inset-0 z-50 hidden w-full place-items-center bg-white/90"
+            :data-room-id="room.roomId"
+            id="status-form-container"
+          >
+            <form action="" @submit="changeStatus($event, room.roomId)" id="status-form">
+              <p>Please select room status:</p>
+              <input type="radio" id="true" name="room-status" value="true" />
+              <label for="html">Occupied</label><br />
+              <input type="radio" id="false" name="room-status" value="false" />
+              <label for="css">Not Occupied</label><br />
+              <button class="mt-4 rounded-md bg-green-500 px-2 py-1 shadow-sm shadow-black/80">
+                Apply
+              </button>
+            </form>
+          </div>
+        </div>
+
         <div class="absolute h-full w-full bg-slate-500/50" v-if="room.isOccupied == 'true'"></div>
         <div class="absolute h-full w-full bg-blue-500/50" v-if="room.isOccupied == 'false'"></div>
         {{ room.roomNumber }} <br />
         {{ room.isOccupied }} <br />
         {{ room.roomId }} <br />
-        <button
-          id="delete-button"
-          class="z-10 cursor-pointer rounded-md bg-red-500 px-2 py-1 text-white shadow-md"
-          @click="() => deleteRoom(userUid, room.roomId)"
-        >
-          deleteRoom
-        </button>
       </li>
     </ul>
   </div>
@@ -27,7 +51,7 @@
 import { ref } from 'vue'
 
 // Firebase imports
-import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore'
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore'
 import { db, auth } from '@/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
@@ -69,6 +93,36 @@ const deleteRoom = async (userId, roomId) => {
     await deleteDoc(doc(db, userId, roomId))
   } catch (err) {
     console.error(err)
+  }
+}
+
+// Change room status function
+const changeStatus = async (e, roomId) => {
+  e.preventDefault()
+  const form = document.querySelector(`[data-room-id="${roomId}"] #status-form`)
+  const roomStatus = form['room-status'].value
+  if (roomStatus === '') return // check if value is empty
+  try {
+    await setDoc(
+      doc(db, userUid.value, roomId),
+      {
+        isOccupied: roomStatus
+      },
+      { merge: true }
+    )
+  } catch (err) {
+    console.error(err)
+  }
+}
+// Change room statuspopup function
+const statusPopup = (roomId) => {
+  const formPopup = document.querySelector(`[data-room-id="${roomId}"]`)
+  if (formPopup.classList.contains('hidden')) {
+    formPopup.classList.remove('hidden')
+    formPopup.classList.add('grid')
+  } else if (formPopup.classList.contains('grid')) {
+    formPopup.classList.remove('grid')
+    formPopup.classList.add('hidden')
   }
 }
 </script>
