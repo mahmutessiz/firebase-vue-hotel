a495919dc5363bb44462a0c4369e26acc09f8095
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-08-27 16:11:23 +0300

    change navbar style

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="w-full">
-    <div class="flex w-full justify-end gap-4 px-4 py-2" v-if="isLoggedIn == true">
+    <div class="flex w-full justify-end gap-4 border-b px-4 py-2" v-if="isLoggedIn == true">
       <button
         class="h-fit w-fit rounded-full hover:shadow-lg hover:shadow-black/50"
         title="add room"
@@ -11,32 +11,32 @@
       <AddOptionsPopup id="popup-menu-add" class="hidden" />
 
       <ul class="flex w-full items-center justify-end gap-4 px-4 py-2">
-        <li class="cursor-pointer text-xl hover:text-blue-500">
+        <li class="rounded-md px-2 py-1 text-lg transition-all duration-200 hover:shadow-md">
           <RouterLink to="/">Home</RouterLink>
         </li>
-        <li class="cursor-pointer text-xl hover:text-blue-500">
-          <RouterLink to="/signup">signup</RouterLink>
-        </li>
-        <li class="cursor-pointer text-xl hover:text-blue-500">
-          <RouterLink to="/login">signin</RouterLink>
-        </li>
         <li>
-          <button class="rounded-md bg-rose-500 px-2 py-1 text-xl" @click="signOutFunc">
-            sign out
+          <button
+            class="rounded-md bg-rose-500 px-2 py-1 hover:shadow-sm hover:shadow-black/50"
+            @click="signOutFunc"
+          >
+            Logout
           </button>
         </li>
       </ul>
     </div>
 
-    <div class="flex w-full justify-end px-4 py-2" v-if="isLoggedIn == false">
+    <div class="flex w-full justify-end border-b px-4 py-2" v-if="isLoggedIn == false">
       <ul class="flex w-full items-center justify-end gap-4 px-4 py-2">
         <li class="cursor-pointer">
           <RouterLink
             to="/"
-            class="rounded-md bg-gray-100 px-2 py-1 text-lg shadow-sm shadow-black/50 transition-all duration-200 hover:shadow-md"
+            class="rounded-md px-2 py-1 text-lg transition-all duration-200 hover:shadow-md"
           >
             Home</RouterLink
           >
+        </li>
+        <li class="rounded-md px-2 py-1 text-lg transition-all duration-200 hover:shadow-md">
+          <RouterLink to="/signup">Sign up</RouterLink>
         </li>
         <li>
           <RouterLink
