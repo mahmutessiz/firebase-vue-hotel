07aaf1581753b908de139eead3b9d7354a6db48d
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-09-22 01:12:34 +0300

    buggy update fix positioning

--- src/components/AddOptionsPopup.vue
+++ src/components/AddOptionsPopup.vue
@@ -1,15 +1,27 @@
 <template>
-  <div
-    class="absolute left-[40%] top-28 z-50 m-auto flex h-60 w-60 flex-col items-center border-2 border-black bg-red-500"
-  >
-    <form action="" class="flex flex-col items-center gap-2">
-      <label for="room-number">Room number</label>
-      <input type="text" id="room-number" /> <br />
+  <div class="">
+    <div
+      class="absolute inset-0 z-[49] grid place-content-center bg-red-900/30"
+      id="close-popup-layer"
+      @click="showHidePopup"
+    ></div>
+    <div
+      class="z-50 m-auto flex flex-col items-center rounded-md border-2 border-black bg-red-500 px-6 py-4"
+    >
+      <form action="" class="isolate z-50 flex flex-col items-center gap-2">
+        <label for="room-number">Room number</label>
+        <input type="text" id="room-number" /> <br />
 
-      <label for="isOccupied">is ocuppied?</label>
-      <input type="text" id="isOccupied" placeholder=" true or false" /> <br />
-      <input type="button" value="Submit" class="border border-black px-2 py-1" @click="addRoom" />
-    </form>
+        <label for="isOccupied">is ocuppied?</label>
+        <input type="text" id="isOccupied" placeholder=" true or false" /> <br />
+        <input
+          type="button"
+          value="Submit"
+          class="border border-black px-2 py-1"
+          @click="addRoom"
+        />
+      </form>
+    </div>
   </div>
 </template>
 
@@ -20,6 +32,12 @@ import { db, auth } from '@/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
 let currentUser = null
+
+// Popup visibility status
+const showHidePopup = () => {
+  const popup = document.querySelector('#popup-menu-add')
+  popup.classList.toggle('hidden')
+}
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
